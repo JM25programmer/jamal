2ee152a81a4b28c7b9b4a54eafcf42a7bd663bed
JM25programmer <jamalafilal68@gmail.com>
2025-08-04 10:40:02 +0100

    degreootpppppeggjgg

---
+++ pro.py
@@ -0,0 +1,14 @@
+print(" kogko")
+#Convert grade to another scale  
+print("Hello, welcome to the grade converter")     
+#Get user input
+score= float(input("enter your score: "))
+total=float(input("enter the total possible score: "))
+convert=float(input("enter the scale you want to convert to: "))
+#Calculate the score as a percentage
+percentage=(score/total)*100
+print("your score is", percentage, "%")
+#Convert the score to the new scale
+score=(score*convert)/total 
+print("your score is", score, "on a scale of", convert)
+fucccccccccccccccccccccccck 
